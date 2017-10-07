4bee2155596a63a9893ce83270dcc4965a077fdc
Adrian Babilinski <ababilinski@gmail.com>
2017-10-07 17:26:44 -0400

    added collision info

--- Assets/_Science-Augmented/Application/Scripts/EducationModel.cs
+++ Assets/_Science-Augmented/Application/Scripts/EducationModel.cs
@@ -5,10 +5,35 @@ using UnityEngine;
 public class EducationModel : MonoBehaviour
 {
 
-    public string Key { get; private set; }
-
+    public string Key;
+    public string CombinedValue;
+    public string Value;
+    public bool Enzyme = false;
+    public GameObject Model;
     public void SetKey(string value)
     {
+        Model = gameObject;
         Key = value;
+    }
+    public void SetValue(string value)
+    {
+        Model = gameObject;
+        Value = value;
+    }
+
+    public void SetCombinedValue(string value)
+    {
+        CombinedValue = value;
+    }
+
+    public void SetModel(GameObject value)
+    {
+        Model = value;
+    }
+
+    public void Set(string value, GameObject model)
+    {
+        Key = value;
+        Model = model;
     }
 }
