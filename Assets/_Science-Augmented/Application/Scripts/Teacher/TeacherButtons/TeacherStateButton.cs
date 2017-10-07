3e0504866cfd4dac5468ba45fac81bfb6d173651
Krystian Babilinski <kbabilinski@gmail.com>
2017-10-07 15:42:42 -0400

    Added teacher Activity and teacher activity buttons

---
+++ Assets/_Science-Augmented/Application/Scripts/Teacher/TeacherButtons/TeacherStateButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+
+[RequireComponent(typeof(Button))]
+public abstract class TeacherStateButton : MonoBehaviour {
+
+	[SerializeField]
+	private TeacherViewState targetState;
+	private Button thisButton;
+
+	// Use this for initialization
+	void Start () {
+		
+		thisButton = GetComponent<Button> ();
+		thisButton.onClick.AddListener (UpdateState);
+		//TeacherView.sInstance.OnChangeState.AddListener (UpdateButtonGraphic);
+
+	}
+
+
+	void UpdateState(){
+		TeacherView.sInstance.ChangeState (targetState);
+	}
+
+
+}
---
+++ Assets/_Science-Augmented/Application/Scripts/Teacher/TeacherView.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Events;
+
+public enum TeacherViewState{Introduction,Game,Challenge}
+
+public class TeacherStateEvent: UnityEvent<TeacherViewState>{}
+
+public class TeacherView : NetworkBehaviour {
+
+	public TeacherActivity currentActivity;
+	public TeacherViewState currentState;
+	public static TeacherView sInstance;
+	[HideInInspector]
+	public TeacherStateEvent OnChangeState = new TeacherStateEvent ();
+
+	void Awake(){
+		sInstance = this;
+	}
+	public void StartInstruction(){
+
+	}
+
+	public void StartGame(){
+
+	}
+
+	public void EndGame(){
+
+	}
+
+	public void Restart(){
+
+	}
+
+	public void ChangeState(TeacherViewState state){
+		currentState = state;
+		OnChangeState.Invoke (state);
+	}
+		
+
+
+}
