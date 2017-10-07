74efdadd59e827fe4442c1eb8bc16bfe5c26233a
Krystian Babilinski <kbabilinski@gmail.com>
2017-10-07 14:14:34 -0400

    added script folder and scene manager

---
+++ Assets/_Science-Augmented/Application/Scripts/NetworkClassroomManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using Prototype.NetworkLobby;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NetworkClassroomManager : NetworkBehaviour {
+
+	static public List<NetworkPlayer> splayers = new List<NetworkPlayer>();
+	static public NetworkClassroomManager sInstance = null;
+
+	public string teacherScene;
+	public string studentString;
+
+	protected bool _running = true;
+
+	void Awake()
+	{
+		sInstance = this;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	[ServerCallback]
+	void Update()
+	{
+		if (!_running)
+			return;
+
+	}
+
+	IEnumerator ReturnToLoby()
+	{
+		_running = false;
+		yield return new WaitForSeconds(3.0f);
+		LobbyManager.s_Singleton.ServerReturnToLobby();
+	}
+
+	public IEnumerator WaitForRespawn(NetworkSpaceship ship)
+	{
+		yield return new WaitForSeconds(4.0f);
+
+		ship.Respawn();
+	}
+}
---
+++ Assets/_Science-Augmented/Application/Scripts/NetworkPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkPlayer : NetworkBehaviour {
+	
+	public bool isTeacher;
+
+	void Awake()
+	{
+		//register the spaceship in the gamemanager, that will allow to loop on it.
+		NetworkClassroomManager.splayers.Add(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
