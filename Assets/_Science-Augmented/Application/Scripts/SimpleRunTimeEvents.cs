59c54d5abcc2944618b13b94bbbec7df18f4c334
Krystian Babilinski <kbabilinski@gmail.com>
2017-10-07 14:30:51 -0400

    DisableVuforiaInMainScene

---
+++ Assets/_Science-Augmented/Application/Scripts/SimpleRunTimeEvents.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class SimpleRunTimeEvents : MonoBehaviour {
+
+	public UnityEvent OnStart;
+	// Use this for initialization
+	void Start () {
+		OnStart.Invoke ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
