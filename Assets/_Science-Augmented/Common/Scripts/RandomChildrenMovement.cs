9a5e2b15b8d255153baf76939ea19c88cb443000
Adrian Babilinski <ababilinski@gmail.com>
2017-10-08 09:32:24 -0400

    improved model animation

---
+++ Assets/_Science-Augmented/Common/Scripts/RandomChildrenMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomChildrenMovement : MonoBehaviour {
+    private float _endRange = 1;
+    private float _startRange = 1.8f;
+    private float _oscillateRange;
+    private float _oscillateOffset;
+    bool fit;
+    List<Vector3> startPostions = new List<Vector3>();
+
+    void Start()
+    {
+        _oscillateRange = ( _endRange - _startRange ) / 10;
+        _oscillateOffset = _oscillateRange + _startRange;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            startPostions.Add(transform.GetChild(i).localPosition);
+        }
+       
+    }
+
+    public void SetRandom()
+    {
+        fit = false;
+    }
+
+    public void SetBack()
+    {
+        fit = true;
+        StartCoroutine(StartSetInPlace());
+    }
+
+    private IEnumerator StartSetInPlace(float time = 2)
+    {
+        fit = true;
+
+        float elapsedTime = 0;
+
+
+        while (elapsedTime < time)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).localPosition = Vector3.Lerp(transform.localPosition, startPostions[i], ( elapsedTime / time ));
+            }
+         
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    void Update()
+    {
+        if(fit)
+            return;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            float xpostion = _oscillateOffset+ Mathf.Sin(Time.time * Random.Range(0, 1f)) * _oscillateRange ;
+            float zpostion = _oscillateOffset +Mathf.Sin(Time.time * Random.Range(0, 17f)) * _oscillateRange ;
+            float ypostion = _oscillateOffset+ Mathf.Sin(Time.time * Random.Range(0, 1f)) * _oscillateRange;
+        
+            transform.GetChild(i).localPosition = startPostions[i] + new Vector3(xpostion, ypostion, zpostion);
+        }
+      
+    }
+}
---
+++ Assets/_Science-Augmented/Common/Scripts/RandomChildrenRotation.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomChildrenRotation : MonoBehaviour {
+    public Vector3 RotationSpeeds = new Vector3(0,0,0);
+    public Vector3 RotationSpeedMin = new Vector3(0, 0, 0);
+    public Vector3 RotationSpeedsMax = new Vector3(0, 0, 0);
+    bool fit;
+    public float ChangeTime = 2f;
+    // Use this for initialization
+    void Start ()
+    {
+        StartRandomRotation();
+    }
+
+    public void SetInPlace(float time = 30)
+    {
+        fit = true;
+        StopAllCoroutines();
+        if(gameObject.activeSelf)
+        StartCoroutine(StartSetInPlace(time));
+    }
+
+    public void StartRandomRotation()
+    {
+        fit = false;
+        if (gameObject.activeSelf)
+            StartCoroutine(ChangeRotation());
+    }
+
+    private IEnumerator StartSetInPlace( float time = 30)
+    {
+        fit = true;
+
+        float elapsedTime = 0;
+       
+
+        while (elapsedTime < time)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).localEulerAngles = Vector3.Lerp(transform.localEulerAngles, Vector3.zero, ( elapsedTime / time ));
+            }
+           
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public IEnumerator ChangeRotation()
+    {
+        RotationSpeeds = new Vector3(Random.Range(RotationSpeedMin.x, RotationSpeedsMax.x), Random.Range(RotationSpeedMin.y, RotationSpeedsMax.y), Random.Range(RotationSpeedMin.z, RotationSpeedsMax.z));
+
+        yield return new WaitForSeconds(ChangeTime);
+        StartCoroutine(ChangeRotation());
+    }
+    // Update is called once per frame
+	void Update ()
+	{
+	    if(fit)
+	    {
+	        return;
+	    }
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).Rotate(Vector3.up * ( RotationSpeeds.y * Time.deltaTime ));
+            transform.GetChild(i).Rotate(Vector3.right * ( RotationSpeeds.x * Time.deltaTime ));
+            transform.GetChild(i).Rotate(Vector3.forward * ( RotationSpeeds.z * Time.deltaTime ));
+        }
+      
+    }
+}
