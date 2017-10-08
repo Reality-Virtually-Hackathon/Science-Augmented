bc3460b1de12f5a79e1a87d51fbdc9d95b469675
Adrian Babilinski <ababilinski@gmail.com>
2017-10-07 21:00:33 -0400

    added models

---
+++ Assets/_Science-Augmented/Common/Scripts/RandomRotation.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour {
+    public Vector3 RotationSpeeds = new Vector3(0,0,0);
+    public Vector3 RotationSpeedMin = new Vector3(0, 0, 0);
+    public Vector3 RotationSpeedsMax = new Vector3(0, 0, 0);
+    bool fit;
+    public float ChangeTime = 2f;
+    // Use this for initialization
+    void Start ()
+    {
+        StartRandomRotation();
+    }
+
+    public void SetInPlace(float time = 2)
+    {
+        fit = true;
+        StopAllCoroutines();
+        StartCoroutine(StartSetInPlace(time));
+    }
+
+    public void StartRandomRotation()
+    {
+        fit = false;
+        StartCoroutine(ChangeRotation());
+    }
+
+    private IEnumerator StartSetInPlace( float time = 2)
+    {
+        fit = true;
+
+        float elapsedTime = 0;
+       
+
+        while (elapsedTime < time)
+        {
+            transform.localEulerAngles =Vector3.Lerp(transform.localEulerAngles, Vector3.zero, ( elapsedTime / time ));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public IEnumerator ChangeRotation()
+    {
+        RotationSpeeds = new Vector3(Random.Range(RotationSpeedMin.x, RotationSpeedsMax.x), Random.Range(RotationSpeedMin.y, RotationSpeedsMax.y), Random.Range(RotationSpeedMin.z, RotationSpeedsMax.z));
+
+        yield return new WaitForSeconds(ChangeTime);
+        StartCoroutine(ChangeRotation());
+    }
+    // Update is called once per frame
+	void Update ()
+	{
+	    if(fit)
+	    {
+	        return;
+	    }
+	    transform.Rotate(Vector3.up * (RotationSpeeds.y* Time.deltaTime));
+        transform.Rotate(Vector3.right * ( RotationSpeeds.x * Time.deltaTime ));
+        transform.Rotate(Vector3.forward * ( RotationSpeeds.z * Time.deltaTime ));
+    }
+}
---
+++ Assets/_Science-Augmented/Common/Scripts/ShowChild.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowChild : MonoBehaviour
+{
+    public int ChildToShow;
+    public bool ShowOnStart =false;
+	// Use this for initialization
+	void Start () {
+	    for (int i = 0; i < transform.childCount; i++)
+	    {
+	        transform.GetChild(i).gameObject.SetActive(false);
+	    }
+	    if (ShowOnStart)
+	    {
+	        ShowChildIndex(ChildToShow);
+	    }
+	}
+
+    public void ShowChildIndex(int index)
+    {
+        int childIndex = index % transform.childCount;
+        transform.GetChild(childIndex).gameObject.SetActive(true);
+    }
+}
